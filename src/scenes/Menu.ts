9380662a23d94d935880713bf953b4d10836345b
james1q2w3e <james1q2w3e@gmail.com>
2022-11-30 14:34:05 -0500

    fixed character movement on menu

--- src/scenes/Menu.ts
+++ src/scenes/Menu.ts
@@ -1,23 +1,37 @@
 import Phaser from "phaser";
+import { createLizardAnims } from '../anims/EnemyAnims'
 import { createCharacterAnims } from '../anims/CharacterAnims'
+import { createChestAnims } from '../anims/TreasureAnims'
 import '../characters/Faune'
 import Faune from '../characters/Faune'
+import Lizard from '../enemies/Lizard'
+import Chest from '../items/Chest'
+import { sceneEvents } from '../events/EventsCenter'
 
 export default class Menu extends Phaser.Scene {
   private cursors!: Phaser.Types.Input.Keyboard.CursorKeys
   private faune!: Faune
+  private knives!: Phaser.Physics.Arcade.Group
+	private lizards!: Phaser.Physics.Arcade.Group
+
+	private playerLizardsCollider?: Phaser.Physics.Arcade.Collider
 
   constructor() {
     super('menu');
   }
-  init() {
-  }
   
-  preLoad() {
+  preload() {
     this.cursors = this.input.keyboard.createCursorKeys()
   }
 
   create() { // creating the menu screen
+
+    createCharacterAnims(this.anims)
+    
+		this.knives = this.physics.add.group({
+			classType: Phaser.Physics.Arcade.Image,
+			maxSize: 200
+		})
 
     let playButton = this.add.text((this.game.renderer.width / 2) *.35, this.game.renderer.height * 0.20, 'Start Game', {fontSize: 30}).setDepth(1)
     let aboutButton = this.add.text((this.game.renderer.width / 2) *.55, this.game.renderer.height * 0.35, 'About Us', {fontSize: 20}).setDepth(1)
@@ -52,22 +66,22 @@ export default class Menu extends Phaser.Scene {
     })
 
     // initialize the player
-    // createCharacterAnims(this.anims)
-    // this.faune = this.add.faune(128, 128, 'faune')
-    // this.faune.setCollideWorldBounds(true) // collides with the world's border
-    // this.faune.setDepth(999) // sets the z-index for the player sprite, (putting it above everything else)
-    // // this.cameras.main.startFollow(this.faune, true)
+    this.faune = this.add.faune(128, 128, 'faune')
+    this.faune.setCollideWorldBounds(true) // collides with the world's border
+    this.faune.setDepth(999) // sets the z-index for the player sprite, (putting it above everything else)
+    this.faune.setKnives(this.knives)
+    // this.cameras.main.startFollow(this.faune, true)
     
     const wallsLayer = map.createStaticLayer('Walls', tileset)
 		wallsLayer.setCollisionByProperty({ collides: true })
     this.physics.add.collider(this.faune, wallsLayer)
 
     
-  }
+	}
   
-  // update(t: number, dt: number) {
-	// 	if (this.faune) {
-	// 		this.faune.update(this.cursors)
-	// 	}
-	// }
+  update(t: number, dt: number) {
+		if (this.faune) {
+			this.faune.update(this.cursors)
+		}
+	}
 }
