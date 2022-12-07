beb0f5dc3cb79c98cb1d4fa5573769265ff2e6f8
Elijah Rhinehardt <elijah.rhinehardt7@gmail.com>
2022-12-07 13:09:56 -0500

    Added scenes and html

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,19 @@
 import Phaser from "phaser";
 
-
 import Preloader from "./scenes/Preloader";
 import Game from "./scenes/Game";
 import GameUI from "./scenes/GameUI";
 import Menu from "./scenes/Menu";
-import Winner from './scenes/Winner'
+import Winner from "./scenes/Winner";
 import Loser from "./scenes/Loser";
+import EnterName from "./scenes/EnterName";
+import LeaderBoard from "./scenes/LeaderBoard";
 
 import PhaserRaycaster from "phaser-raycaster";
 
 export default new Phaser.Game({
   type: Phaser.AUTO,
-  parent: 'phaser',
+  parent: "phaser",
   width: 400,
   height: 250,
   physics: {
@@ -22,17 +23,13 @@ export default new Phaser.Game({
       debug: true,
     },
   },
-  scene: [
-		Preloader,
-		Game,
-		GameUI,
-		Menu,
-    Winner,
-    Loser
-	],
+  scene: [Preloader, Game, GameUI, Menu, Winner, Loser, EnterName, LeaderBoard],
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  dom: {
+    createContainer: true,
   },
   plugins: {
     scene: [
