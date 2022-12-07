6326ca0010855c80acb9d2bd36238c4d46fbee74
Elijah Rhinehardt <elijah.rhinehardt7@gmail.com>
2022-12-07 10:40:30 -0500

    Fix HTML not working

--- src/scenes/EnterName.ts
+++ src/scenes/EnterName.ts
@@ -8,7 +8,7 @@ export default class EnterName extends Phaser.Scene {
   }
 
   preload() {
-    this.load.html("nameform", "nameForm.html");
+    this.load.html("nameform", "/nameForm.html");
   }
 
   create() {
@@ -18,21 +18,25 @@ export default class EnterName extends Phaser.Scene {
       this.cameras.main.worldView.y + this.cameras.main.height / 2;
 
     this.exitTime = this.scene.settings.data.currentTime;
+    let form = `
+<input type="text" name="nameField" placeholder="Enter your name" style="font-size: 32px">
+<input type="button" name="playButton" value="Let's Play" style="font-size: 32px">
+`;
 
     // this.add
     //   .dom(
-    //     400,
-    //     300,
+    //     100,
+    //     100,
     //     "div",
     //     "background-color:lime;width:220px;height:100px;font:48px Arial",
     //     "Phaser"
     //   )
     //   .setDepth(10);
-    const text1 = this.add
-      .text(screenCenterX, screenCenterY, `Your score was ${this.exitTime}`)
-      .setOrigin(0.5);
-    text1.setTint(0xff00ff, 0xff0000, 0xff00ff, 0xff0000);
-    // let element = this.add.dom(100, 100).createFromCache("nameform");
+    // const text1 = this.add
+    //   .text(screenCenterX, screenCenterY, `Your score was ${this.exitTime}`)
+    //   .setOrigin(0.5);
+    // text1.setTint(0xff00ff, 0xff0000, 0xff00ff, 0xff0000);
+    let element = this.add.dom(150, 150).createFromCache("nameform");
     // console.log(this.scene)
   }
 
