124d6ec243deec20fbf245f8782a3530bfb9da7b
thingung <88118540+thingung@users.noreply.github.com>
2022-12-07 11:54:32 -0500

    Revert "Leaderboard"

--- src/scenes/Loser.ts
+++ src/scenes/Loser.ts
@@ -1,31 +1,33 @@
 import Phaser from "phaser";
 
 export default class Loser extends Phaser.Scene {
-  private exitTime = 0;
 
-  constructor() {
-    super("loser");
-    // console.log(this)
-  }
+    private exitTime = 0
+  
 
-  preload() {
-    // console.log(this)
-  }
+    constructor() {
+		super('loser')
+        // console.log(this)
+	}
 
-  create() {
-    const screenCenterX =
-      this.cameras.main.worldView.x + this.cameras.main.width / 2;
-    const screenCenterY =
-      this.cameras.main.worldView.y + this.cameras.main.height / 2;
-    // const loadingText = this.add.text(screenCenterX, screenCenterY, 'Loading: 0%').setOrigin(0.5);
+	preload() {
+        // console.log(this)
+	}
 
-    this.exitTime = this.scene.settings.data.currentTime;
+	create() {
 
-    const text1 = this.add
-      .text(screenCenterX, screenCenterY, `You done lost son`)
-      .setOrigin(0.5);
-    text1.setTint(0xff00ff, 0xff0000, 0xff00ff, 0xff0000);
-  }
+        const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
+        const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
+        // const loadingText = this.add.text(screenCenterX, screenCenterY, 'Loading: 0%').setOrigin(0.5);
 
-  update() {}
+        this.exitTime = this.scene.settings.data.currentTime
+        
+        const text1 = this.add.text(screenCenterX, screenCenterY, `You done lost son`).setOrigin(0.5)
+        text1.setTint(0xff00ff, 0xff0000, 0xff00ff, 0xff0000);
+        // console.log(this.scene)
+    }
+
+    update() {
+    
+    }
 }
